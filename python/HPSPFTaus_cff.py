d3a234d62c120c6369be388765091070360d787b
Christian Veelken <sha1-5c72da6f595cce9b6b48aff6d56f01e9beb4aad1@cern.ch>
2013-05-02 11:51:41 +0000

    improved syntax of strip Pt > 2.5 GeV cut

--- python/HPSPFTaus_cff.py
+++ python/HPSPFTaus_cff.py
@@ -670,7 +670,7 @@ hpsPFTauProducerSansRefs = cms.EDProducer(
         cms.PSet(
             name = cms.string("leadStripPtLt2_5"),
             plugin = cms.string("RecoTauStringCleanerPlugin"),
-            selection = cms.string("signalPiZeroCandidates().size() = 0 | signalPiZeroCandidates()[0].pt > 2.5"),
+            selection = cms.string("signalPiZeroCandidates().size() = 0 | signalPiZeroCandidates().at(0).pt() > 2.5"),
             selectionPassFunction = cms.string("0"),
             selectionFailValue = cms.double(1e3)
         ),
